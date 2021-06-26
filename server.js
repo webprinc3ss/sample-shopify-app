33836eb874406e9caed0a4b82dce1b6e0fd2f875
Mystic Kitty <djabranton@gmail.com>
2021-06-26 16:03:48 -0700

    Created store and added app to store.

--- server.js
+++ server.js
@@ -1,50 +1,69 @@
 require('isomorphic-fetch');
+const dotenv = require('dotenv');
 const Koa = require('koa');
 const next = require('next');
-const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
-const dotenv = require('dotenv');
-const { verifyRequest } = require('@shopify/koa-shopify-auth');
-const session = require('koa-session');
+const {default: createShopifyAuth} = require('@shopify/koa-shopify-auth');
+const {verifyRequest} = require('@shopify/koa-shopify-auth');
+const {default: Shopify, ApiVersion} = require('@shopify/shopify-api');
+const Router = require('koa-router');
 
 dotenv.config();
 
+Shopify.Context.initialize({
+  API_KEY: process.env.SHOPIFY_API_KEY,
+  API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
+  SCOPES: process.env.SHOPIFY_API_SCOPES.split(","),
+  HOST_NAME: process.env.SHOPIFY_APP_URL.replace(/https:\/\//, ""),
+  API_VERSION: ApiVersion.October20,
+  IS_EMBEDDED_APP: true,
+  SESSION_STORAGE: new Shopify.Session.MemorySessionStorage(),
+});
+
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
+const app = next({dev: dev});
 const handle = app.getRequestHandler();
 
-const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY } = process.env;
+const ACTIVE_SHOPIFY_SHOPS = {};
 
 app.prepare().then(() => {
   const server = new Koa();
-  server.use(session({ sameSite: 'none', secure: true }, server));
-  server.keys = [SHOPIFY_API_SECRET_KEY];
+  const router = new Router();
+  server.keys = [Shopify.Context.API_SECRET_KEY];
 
   server.use(
     createShopifyAuth({
-      apiKey: SHOPIFY_API_KEY,
-      secret: SHOPIFY_API_SECRET_KEY,
-      scopes: [
-        'read_products',
-        'write_products',
-        'read_script_tags',
-        'write_script_tags'
-        ],
       afterAuth(ctx) {
-        const { shop, accessToken } = ctx.session;
+        const {shop, scope} = ctx.state.shopify;
+        ACTIVE_SHOPIFY_SHOPS[shop] = scope;
 
-        ctx.redirect('/');
+        ctx.redirect(`/?shop=${shop}`);
       },
     }),
   );
 
-  server.use(verifyRequest());
-  server.use(async (ctx) => {
+  const handleRequest = async (ctx) => {
     await handle(ctx.req, ctx.res);
     ctx.respond = false;
     ctx.res.statusCode = 200;
+  };
 
+  router.get("/", async (ctx) => {
+    const shop = ctx.query.shop;
+
+    if (ACTIVE_SHOPIFY_SHOPS[shop] === undefined) {
+      ctx.redirect(`/auth?shop=${shop}`);
+    } else {
+      await handleRequest(ctx);
+    }
   });
+
+  router.get("(/_next/static/.*)", handleRequest);
+  router.get("/_next/webpack-hmr", handleRequest);
+  router.get("(.*)", verifyRequest(), handleRequest);
+
+  server.use(router.allowedMethods());
+  server.use(router.routes());
 
   server.listen(port, () => {
     console.log(`> Ready on http://localhost:${port}`);
